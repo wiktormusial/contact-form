e7f427e682e6433b95d03e704ca01cad3de13244
Wiktor Musial <wiktormusial@icloud.com>
2022-01-21 23:40:40 +0100

    Add Contact app

--- backend/mailer/settings.py
+++ backend/mailer/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'contact.apps.ContactConfig'
+    'contact.app.ContactAppConfig'
 ]
 
 MIDDLEWARE = [
