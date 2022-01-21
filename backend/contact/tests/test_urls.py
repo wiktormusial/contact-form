0ed716f7ca25e05bec1e5a465df919975ba54b9e
Wiktor Musial <wiktormusial@icloud.com>
2022-01-21 23:27:36 +0100

    Write test for index URL

---
+++ backend/contact/tests/test_urls.py
@@ -0,0 +1,11 @@
+from django.test import TestCase
+from django.test import Client
+from django.urls import reverse
+
+c = Client()
+
+
+class URLTestCase(TestCase):
+    def test_view_url_exists_at_desired_location(self):
+        response = c.get(reverse('index'))
+        self.assertEqual(response.status_code, 200)
