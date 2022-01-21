7f60f9d9f30e19bbc4a4d6c3d79f9d90d8c71406
Wiktor Musial <wiktormusial@icloud.com>
2022-01-21 23:41:27 +0100

    Update settings.py

--- backend/mailer/settings.py
+++ backend/mailer/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['/templates']
+ALLOWED_HOSTS = ['']
 
 
 # Application definition
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'mailer.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
