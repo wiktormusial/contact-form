4dad6c1482411e6df3afea38c7ba52875724d64a
Wiktor Musial <wiktormusial@icloud.com>
2022-01-22 01:12:58 +0100

    Create MailerSettings Model

--- backend/contact/models.py
+++ backend/contact/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class MailerSettings(models.Model):
+    EMAIL_HOST = models.CharField(max_length=128)
+    EMAIL_PORT = models.IntegerField()
+    EMAIL_HOST_USER = models.EmailField(max_length=254)
+    EMAIL_HOST_PASSWORD = models.CharField(max_length=254)
+
+    def __str__(self):
+        return self.EMAIL_HOST
+
+    def save(self, *args, **kwargs):
+        if not self.pk and MailerSettings.objects.exists():
+            pass
+        else:
+            return super(MailerSettings, self).save(*args, **kwargs)
