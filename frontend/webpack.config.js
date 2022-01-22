e9c44b4dd25b2eb0d975a3ce356cb19e815c9cc6
Wiktor Musial <wiktormusial@icloud.com>
2022-01-23 00:43:17 +0100

    Update webpack config

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -21,7 +21,10 @@ module.exports = {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         loader: "babel-loader",
-        options: { presets: ["@babel/preset-env", "@babel/preset-react"] }
+        options: {
+          presets: ["@babel/preset-env", "@babel/preset-react"], 
+          plugins: ["@babel/plugin-transform-runtime"]
+        }
       },
     ]
   }
