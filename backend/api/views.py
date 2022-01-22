a24d87b413584a734a3a4c197d9762de00cf8e1c
Wiktor Musial <wiktormusial@icloud.com>
2022-01-22 16:07:13 +0100

    Write SendMailView

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+
+class SendMailView(APIView):
+    """
+    Send an email using mailgun to client email address
+    """
+
+    def get(self, request, format=None):
+        return Response('Ok')
+
+    def post(self, request, format=None):
+        return Response('Ok')
