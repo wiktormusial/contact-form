0d0fea01eceb91725d5b273e0d0cc6dd9e28497c
Wiktor Musial <wiktormusial@icloud.com>
2022-01-22 02:45:13 +0100

    Create webpack config

---
+++ frontend/webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path')
+const ESLintPlugin = require('eslint-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'index-bundle.js',
+    path: path.resolve(path.normalize('../backend/'), './static/js'),
+  },
+  plugins: [
+    new ESLintPlugin()
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+        options: { presets: ["@babel/preset-env", "@babel/preset-react"] }
+      },
+    ]
+  }
+}
