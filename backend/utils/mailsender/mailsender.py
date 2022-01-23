57f232787564931b429d4100e01c9f0cddc12f6e
Wiktor Musial <wiktormusial@icloud.com>
2022-01-23 14:22:51 +0100

    Update MailSender

--- backend/utils/mailsender/mailsender.py
+++ backend/utils/mailsender/mailsender.py
@@ -17,17 +17,29 @@ class MailSender:
         print(self.password)
 
     def server_connect(self):
-        server = smtplib.SMTP_SSL(self.server, self.port)
+        server = smtplib.SMTP_SSL(self.server, self.port, timeout=10)
         server.login(user=self.username,
                      password=self.password)
 
         return server
 
+    def check_connection(self):
+        try:
+            self.server_connect().noop()
+            return "Ok"
+        except TimeoutError:
+            return "Timeout error"
+        except smtplib.SMTPAuthenticationError:
+            return "Auth error"
+
     def write(self, title, recipient, content):
-        msg = EmailMessage()
-        msg['Subject'] = title
-        msg['To'] = recipient
-        msg.set_content(content)
-        self.server_connect().send_message(msg)
+        if self.check_connection() == "Ok":
+            msg = EmailMessage()
+            msg['Subject'] = title
+            msg['To'] = recipient
+            msg.set_content(content)
+            self.server_connect().send_message(msg)
+        else:
+            print('Error')
 
         return True
