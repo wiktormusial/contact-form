3f84766d7a9d9c1e296034c8e0f54174b937ec02
Wiktor Musial <wiktormusial@icloud.com>
2022-01-25 18:07:09 +0100

    Create editor URL

--- backend/contact/urls.py
+++ backend/contact/urls.py
@@ -3,5 +3,6 @@ from .views import ContactView, ConfigureView
 
 urlpatterns = [
     path('', ContactView.as_view(), name="index"),
-    path('configure', ConfigureView.as_view(), name="configure")
+    path('configure', ConfigureView.as_view(), name="configure"),
+    path('editor', EditorView.as_view(), name="edit")
 ]
