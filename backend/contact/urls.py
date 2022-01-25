72d9781f70219f4b3d3663ecccfee9c284a97549
Wiktor Musial <wiktormusial@icloud.com>
2022-01-25 20:29:30 +0100

    Create Url for Editor

--- backend/contact/urls.py
+++ backend/contact/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import ContactView, ConfigureView, EditorView
+from .views import ContactView, ConfigureView, EditorView, EditorCategoryDeleteView
 
 urlpatterns = [
     path('', ContactView.as_view(), name="index"),
     path('configure', ConfigureView.as_view(), name="configure"),
-    path('editor', EditorView.as_view(), name="edit")
+    path('editor', EditorView.as_view(), name="edit"),
+    path('cat/<int:pk>/delete',
+         EditorCategoryDeleteView.as_view(),
+         name="deletecategory")
 ]
